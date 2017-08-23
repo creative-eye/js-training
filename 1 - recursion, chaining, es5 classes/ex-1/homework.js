89760af83b37f9241dece94f69d8f21c83909a1f
EVERYMATRIX\teodora.cailean <teodora.cailean@everymatrix.com>
2017-08-23 10:58:30 +0300

    Homework - code review

--- 1 - recursion, chaining, es5 classes/ex-1/homework.js
+++ 1 - recursion, chaining, es5 classes/ex-1/homework.js
@@ -1,12 +1,18 @@
 var Homework = function () {
-this.a = '';
-this.b='';
+    this.a = null;
+    this.b =null;
 };
 
 Homework.prototype.range = function (a, b) {
+    this.a =a;
+    this.b =b;
     const array = [];
-    for (a; a <= b; a++) {
-        array.push(a);
+    if (a > b) {
+        this.a = b;
+        this.b = a;
+    }
+    for (this.a; this.a <= this.b; this.a++) {
+        array.push(this.a);
     }
     return array;
 };
@@ -17,38 +23,44 @@ Homework.prototype.sumOfRange = function (array) {
     }, 0);
 };
 
-Homework.prototype.rubySelect = function (array, newArray) {
-    if (array.length) {
-        if (array[0] % 2 === 0) {
-            newArray.push(array[0]);
+Homework.prototype.rubySelect = function (array) {
+    var index = array.indexOf(array[i]);
+    if (index > -1) {
+        if (array[i] % 2 !== 0) {
+            array.splice(index, 1);
+        } else {
+            i++;
         }
-        array.shift();
     } else {
-        return newArray;
+        return array;
     }
-    return this.rubySelect(array, newArray);
+    return this.rubySelect(array);
 };
 
-Homework.prototype.rubyDropWhile = function (array, newArray) {
-    if (array.length) {
-        if (array[0] > 2) {
-            newArray.push(array[0]);
+Homework.prototype.rubyDropWhile = function (array) {
+    var index = array.indexOf(array[j]);
+    if (index > -1) {
+        if (array[j] > 3) {
+            j++;
+        } else {
+            array.splice(index, 1);
         }
-        array.shift();
     } else {
-        return newArray;
+        return array;
     }
-    return this.rubyDropWhile(array, newArray);
+    return this.rubyDropWhile(array);
 };
 
-var range =new Homework().range(1, 6);
+var range = new Homework().range(10, 6);
 console.log('range', range);
 
 var sum = new Homework().sumOfRange(range);
 console.log('sum', sum);
 
-var select = new Homework().rubySelect([1, 2, 3, 4, 5, 6], []);
-console.log('select',select);
+var i = 0;
+var select = new Homework().rubySelect([1, 2, 3, 4, 5, 6]);
+console.log('select', select);
 
-var dropwhile = new Homework().rubyDropWhile([1, 2, 3, 4, 5, 6, 7], []);
-console.log('dropwhile',dropwhile);
+var j =0;
+var dropWhile = new Homework().rubyDropWhile([1, 2, 3, 4, 5, 6, 7]);
+console.log('dropWhile', dropWhile);

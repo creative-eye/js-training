b2dc7c8fb26f96aec1bd66de57df70994521b34c
morarasu-alexandru <morarasu_alexandru@yahoo.com>
2017-08-29 23:57:38 +0300

    first commit for execrise 2, work in progress

---
+++ 2 - functional intro, data manipulation/src/js/main.js
@@ -0,0 +1,22 @@
+const games = [
+    { name: 'alibaba', type: 'pacanele', jackpot: false },
+    { name: 'alibaba2', type: 'live', jackpot: false },
+    { name: 'gonzo cautare', type: 'poker', jackpot: false },
+    { name: 'alibaba', type: 'live', jackpot: true },
+    { name: 'dracula', type: 'pacanale', jackpot: false },
+    { name: 'dracula', type: 'pacanale', jackpot: true,
+        children: [{ name: 'cosmic fortune', type: 'pacanele', jackpot: true }, { name: 'gonzo cautare', type: 'poker', jackpot: false }] }
+];
+const target = document.getElementById('target');
+const search = document.getElementById('search');
+
+(function(){
+    games.forEach( game => {
+        target.insertAdjacentHTML('beforeend', `<li data-type="${game.type}" data-jackpot="${game.jackpot}">${game.name}</li>`);
+    });
+})();
+
+search.addEventListener('keyup', function() {
+    let searchInput = this.value;
+    console.log(searchInput);
+});

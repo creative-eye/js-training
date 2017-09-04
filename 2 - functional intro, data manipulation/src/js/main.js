8ba9708770f8cc87f162993c7a0a7a8e9b2ecc02
morarasu-alexandru <morarasu_alexandru@yahoo.com>
2017-09-04 23:28:23 +0300

    dev

--- 2 - functional intro, data manipulation/src/js/main.js
+++ 2 - functional intro, data manipulation/src/js/main.js
@@ -1,45 +1,51 @@
 const games = [
-	{name: 'alibaba', type: 'pacanele', jackpot: false},
-	{name: 'alibaba2', type: 'live', jackpot: false},
-	{name: 'gonzo cautare', type: 'poker', jackpot: false},
-	{name: 'alibaba', type: 'live', jackpot: true},
-	{name: 'dracula', type: 'pacanale', jackpot: false},
-	{
-		name: 'dracula', type: 'pacanale', jackpot: true,
-		children: [{name: 'cosmic fortune', type: 'pacanele', jackpot: true}, {
-			name: 'gonzo cautare',
-			type: 'poker',
-			jackpot: false
-		}]
-	}
+    {name: 'alibaba', type: 'pacanele', jackpot: false},
+    {name: 'alibaba2', type: 'live', jackpot: false},
+    {name: 'gonzo cautare', type: 'poker', jackpot: false},
+    {name: 'alibaba', type: 'live', jackpot: true},
+    {name: 'dracula', type: 'pacanale', jackpot: false},
+    {
+        name: 'dracula', type: 'pacanale', jackpot: true,
+        children: [{name: 'cosmic fortune', type: 'pacanele', jackpot: true}, {
+            name: 'gonzo cautare',
+            type: 'poker',
+            jackpot: false
+        }]
+    }
 ];
 const target = document.getElementById('target');
-const search = document.getElementById('searchname');
+const searchname = document.getElementById('searchname');
+const searchtype = document.getElementById('searchtype');
 
-let flattengames = games.reduce(
-	 (acumulator, item) => {
-		if (!item.children) {
-			return acumulator.concat(item);
-		} else {
-			return acumulator.concat(item, item.children);
-		}
-	},
-	[]
+const flattengames = games.reduce(
+    (acumulator, item) => {
+        if (!item.children) {
+            return acumulator.concat(item);
+        } else {
+            return acumulator.concat(item, item.children);
+        }
+    },
+    []
 );
 
-let flattened = [[0, 1], [2, 3], [4, 5]].reduce(
-	(acc, cur) => acc.concat(cur),
-	[]
-);
+flattengames.forEach(game => {
+    target.insertAdjacentHTML('beforeend', `<li data-type="${game.type}" data-jackpot="${game.jackpot}">${game.name}</li>`);
+});
 
 console.log(flattengames);
 
-flattengames.forEach(game => {
-	target.insertAdjacentHTML('beforeend', `<li data-type="${game.type}" data-jackpot="${game.jackpot}">${game.name}</li>`);
+searchname.addEventListener('keyup', function () {
+    let searchInput = this.value;
+    let filteredbyname = flattengames.filter(game => {
+        if (game.name === searchInput) {
+            return game;
+        }}
+    );
+    console.log(filteredbyname);
+
+});
+searchtype.addEventListener('keyup', function () {
+    let searchInput = this.value;
+    console.log(searchInput);
 });
 
-
-search.addEventListener('keyup', function () {
-	let searchInput = this.value;
-	console.log(searchInput);
-});

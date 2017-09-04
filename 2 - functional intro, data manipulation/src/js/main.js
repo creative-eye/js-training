96724280302de575adc144134d34f3f2ed68ed09
alexandru-morarasu <morarasu_alexandru@yahoo.com>
2017-09-04 18:35:59 +0300

    dev dev bla bla

--- 2 - functional intro, data manipulation/src/js/main.js
+++ 2 - functional intro, data manipulation/src/js/main.js
@@ -1,27 +1,45 @@
 const games = [
-    {name: 'alibaba', type: 'pacanele', jackpot: false},
-    {name: 'alibaba2', type: 'live', jackpot: false},
-    {name: 'gonzo cautare', type: 'poker', jackpot: false},
-    {name: 'alibaba', type: 'live', jackpot: true},
-    {name: 'dracula', type: 'pacanale', jackpot: false},
-    {
-        name: 'dracula', type: 'pacanale', jackpot: true,
-        children: [{name: 'cosmic fortune', type: 'pacanele', jackpot: true}, {
-            name: 'gonzo cautare',
-            type: 'poker',
-            jackpot: false
-        }]
-    }
+	{name: 'alibaba', type: 'pacanele', jackpot: false},
+	{name: 'alibaba2', type: 'live', jackpot: false},
+	{name: 'gonzo cautare', type: 'poker', jackpot: false},
+	{name: 'alibaba', type: 'live', jackpot: true},
+	{name: 'dracula', type: 'pacanale', jackpot: false},
+	{
+		name: 'dracula', type: 'pacanale', jackpot: true,
+		children: [{name: 'cosmic fortune', type: 'pacanele', jackpot: true}, {
+			name: 'gonzo cautare',
+			type: 'poker',
+			jackpot: false
+		}]
+	}
 ];
 const target = document.getElementById('target');
-const search = document.getElementById('search');
+const search = document.getElementById('searchname');
 
+let flattengames = games.reduce(
+	 (acumulator, item) => {
+		if (!item.children) {
+			return acumulator.concat(item);
+		} else {
+			return acumulator.concat(item, item.children);
+		}
+	},
+	[]
+);
 
-games.forEach(game => {
-    target.insertAdjacentHTML('beforeend', `<li data-type="${game.type}" data-jackpot="${game.jackpot}">${game.name}</li>`);
+let flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+	(acc, cur) => acc.concat(cur),
+	[]
+);
+
+console.log(flattengames);
+
+flattengames.forEach(game => {
+	target.insertAdjacentHTML('beforeend', `<li data-type="${game.type}" data-jackpot="${game.jackpot}">${game.name}</li>`);
 });
 
+
 search.addEventListener('keyup', function () {
-    let searchInput = this.value;
-    console.log(searchInput);
+	let searchInput = this.value;
+	console.log(searchInput);
 });

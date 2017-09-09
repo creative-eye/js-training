c3f2b04962ac8d5d6cea7e306921535e06573f5e
morarasu-alexandru <morarasu_alexandru@yahoo.com>
2017-09-09 15:28:35 +0300

    improved code

--- 2 - functional intro, data manipulation/src/js/main.js
+++ 2 - functional intro, data manipulation/src/js/main.js
@@ -9,33 +9,33 @@
 
 /*Variables*/
 const games = [
-	{name: 'alibaba', type: 'pacanele', jackpot: false},
-	{name: 'alibaba2', type: 'live', jackpot: false},
-	{name: 'gonzo cautare', type: 'poker', jackpot: false},
-	{name: 'alibaba', type: 'live', jackpot: true},
-	{name: 'dracula', type: 'pacanale', jackpot: false},
-	{
-		name: 'dracula', type: 'pacanale', jackpot: true,
-		children: [{name: 'cosmic fortune', type: 'pacanele', jackpot: true}, {
-			name: 'gonzo cautare',
-			type: 'poker',
-			jackpot: false
-		}]
-	}
+    {name: 'alibaba', type: 'pacanele', jackpot: false},
+    {name: 'alibaba2', type: 'live', jackpot: false},
+    {name: 'gonzo cautare', type: 'poker', jackpot: false},
+    {name: 'alibaba', type: 'live', jackpot: true},
+    {name: 'dracula', type: 'pacanale', jackpot: false},
+    {
+        name: 'dracula', type: 'pacanale', jackpot: true,
+        children: [{name: 'cosmic fortune', type: 'pacanele', jackpot: true}, {
+            name: 'gonzo cautare',
+            type: 'poker',
+            jackpot: false
+        }]
+    }
 ];
 const target = document.getElementById('target');
 const searchname = document.getElementById('searchname');
 const searchtype = document.getElementById('searchtype');
 const jackpot = document.getElementById('jackpot');
 const flattenGames = games.reduce(
-	(acumulator, item) => {
-		if (!item.children) {
-			return acumulator.concat(item);
-		} else {
-			return acumulator.concat(item, item.children);
-		}
-	},
-	[]
+    (acumulator, item) => {
+        if (!item.children) {
+            return acumulator.concat(item);
+        } else {
+            return acumulator.concat(item, item.children);
+        }
+    },
+    []
 );
 
 let searchName = '';
@@ -44,55 +44,61 @@ let filteredGames = flattenGames.slice(0);
 let jackpotCheck = false;
 
 /*Initial Game Loade game*/
-for(i=0; i < filteredGames.length; i++) {
-	console.log(i);
-}
+elementInsertToDom();
 
-filteredGames.forEach(game => {
-	target.insertAdjacentHTML('beforeend', `<li data-type="${game.type}" data-jackpot="${game.jackpot}">${game.name}</li>`);
-});
 
 /*Event listeners*/
 searchname.addEventListener('keyup', function () {
-	searchName = this.value;
-	Search();
+    searchName = this.value;
+    Search();
 });
 searchtype.addEventListener('keyup', function () {
-	searchType = this.value;
-	Search();
+    searchType = this.value;
+    Search();
 });
 
 jackpot.addEventListener('click', function () {
-	jackpotCheck = !jackpotCheck;
-	Search();
+    jackpotCheck = !jackpotCheck;
+    Search();
 });
 
 /*Custom made functions*/
 function FilterGames(arr, apliedfilter, input) {
-		return arr.filter(game => game[apliedfilter].toLowerCase().indexOf(input.toLowerCase()) > -1);
+    return arr.filter(game => game[apliedfilter].toLowerCase().indexOf(input.toLowerCase()) > -1);
 }
 
 function FilterJackPot(arr, input) {
-
-	if(input === true) {
-		return arr.filter(game => game.jackpot )
-	}
-	return arr;
+    if (input === true) {
+        return arr.filter(game => game.jackpot)
+    }
+    return arr;
 }
 
 function Search() {
-	filteredGames = flattenGames.slice(0);
+    filteredGames = flattenGames.slice(0);
 
-	filteredGames = FilterJackPot(filteredGames, jackpotCheck);
-	filteredGames = FilterGames(filteredGames, 'name', searchName);
-	filteredGames = FilterGames(filteredGames, 'type', searchType);
+    filteredGames = FilterJackPot(filteredGames, jackpotCheck);
+    filteredGames = FilterGames(filteredGames, 'name', searchName);
+    filteredGames = FilterGames(filteredGames, 'type', searchType);
 
-	while (target.hasChildNodes()) {
-		target.removeChild(target.firstChild);
-	}
-	filteredGames.forEach(game => {
-		target.insertAdjacentHTML('beforeend', `<li data-type="${game.type}" data-jackpot="${game.jackpot}">${game.name}</li>`);
-	});
+    while (target.hasChildNodes()) {
+        target.removeChild(target.firstChild);
+    }
+    elementInsertToDom()
+}
+
+function elementInsertToDom() {
+    let temp;
+    for (i = 0; i < filteredGames.length; i++) {
+        if (temp) {
+            temp = temp + `<li data-type="${filteredGames[i].type}" data-jackpot="${filteredGames[i].jackpot}">${filteredGames[i].name}</li>`;
+        } else {
+            temp = `<li data-type="${filteredGames[i].type}" data-jackpot="${filteredGames[i].jackpot}">${filteredGames[i].name}</li>`;
+        }
+    }
+    if (temp) {
+        target.insertAdjacentHTML('beforeend', temp);
+    }
 }
 
 /*-----------------------------------*/
@@ -101,19 +107,29 @@ function Search() {
 
 /*Variables*/
 const peopleArray = [
-	{id: 123, name: 'dave', age: 23},
-	{id: 456, name: 'chris', age: 23},
-	{id: 789, name: 'bob', age: 23},
-	{id: 101, name: 'tom', age: 23},
-	{id: 102, name: 'tim', age: 23}
+    {id: 123, name: 'dave', age: 23},
+    {id: 456, name: 'chris', age: 23},
+    {id: 789, name: 'bob', age: 23},
+    {id: 101, name: 'tom', age: 23},
+    {id: 102, name: 'tim', age: 23}
 ];
 const secondTarget = document.getElementById('secondTarget');
 const thirdTarget = document.getElementById('thirdTarget');
 
-peopleArray.forEach(user => {
-    secondTarget.insertAdjacentHTML('beforeend', `<option value="${user.id}">${user.name +' ' + user.age}</option>`)
-});
+(function(){
+    let temp;
+    for (i = 0; i < peopleArray.length; i++) {
+        if (temp) {
+            temp = temp + `<option value="${peopleArray[i].id}">${peopleArray[i].name + ' ' + peopleArray[i].age}</option>`;
+        } else {
+            temp = `<option value="${peopleArray[i].id}">${peopleArray[i].name + ' ' + peopleArray[i].age}</option>`;
+        }
+    }
+    if (temp) {
+        secondTarget.insertAdjacentHTML('beforeend', temp);
+    }
+})();
 
-secondTarget.addEventListener('change', function(){
-	thirdTarget.textContent = this.value;
+secondTarget.addEventListener('change', function () {
+    thirdTarget.textContent = this.value;
 });
